a8d511f464b9fd9ee8f09c512fca010d873c5384
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-10-22 12:41:30 +0530

    website hosting in Github pages

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+
+  background(51);
+  textAlign(CENTER);
+  // stroke(255);
+  // noFill();
+  // strokeWeight(4);
+  textSize(34);
+  text('Kavin Bharathi', width / 2, height / 2);
+}
